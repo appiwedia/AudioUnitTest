6acba189e628036f808e5cccd9c5b3f4efe41202
Mickaël MAS <appiwedia@gmail.com>
2023-03-17 21:13:05 +0100

    Initial Commit

---
+++ FlowerAudioUnitTest/FlowerAudioUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerAudioUnitTestApp.swift
+//  FlowerAudioUnitTest
+//
+//  Created by Mickael Mas on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerAudioUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
